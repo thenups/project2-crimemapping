6d7fbec97ecbc8794694fe34831aa1d62cfd9968
thenups <nn.mathur@gmail.com>
2018-03-19 20:03:07 -0700

    app architechture

---
+++ app.py
@@ -0,0 +1,14 @@
+#################################################
+# Dependencies
+#################################################
+import numpy as np
+
+from flask import Flask, render_template, jsonify, redirect
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine, func, inspect, Column, Integer, String
+
+import os
+from flask_sqlalchemy import SQLAlchemy
